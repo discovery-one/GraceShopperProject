b1a0d52261079390830aa8a7eeb58ab6a2885f45
Annie Moosher <annie.sophia219@gmail.com>
2021-04-21 16:38:45 -0400

    fix(models): code 1 review revisions

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,6 +1,14 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require("sequelize");
+const db = require("../db");
 
-const Order = db.define('order', {});
+const Order = db.define("order", {
+  totalPrice: {
+    type: Sequelize.INTEGER,
+  },
+  status: {
+    type: Sequelize.ENUM("complete", "incomplete"),
+    defaultValue: "incomplete",
+  },
+});
 
 module.exports = Order;
