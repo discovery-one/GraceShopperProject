778a64ec7f556e06e7139b7c0e7bf9e10978af40
Kathy Son <kson1128@gmail.com>
2021-04-22 13:00:12 -0400

    feat(allProducts added to rootReducer)

--- client/store/redux/rootReducer.js
+++ client/store/redux/rootReducer.js
@@ -1,10 +1,11 @@
-import { combineReducers } from "redux";
-import singleProductReducer from "./singleProduct";
-import authReducer from "./auth";
-
+import { combineReducers } from 'redux';
+import singleProductReducer from './singleProduct';
+import authReducer from './auth';
+import productsReducer from './products';
 const rootReducer = combineReducers({
   product: singleProductReducer,
   auth: authReducer,
+  products: productsReducer,
 });
 
 export default rootReducer;
