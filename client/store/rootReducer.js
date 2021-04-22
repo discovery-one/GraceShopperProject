115159bf6ad71656a82303e1730df80e90b5c5df
Annie Moosher <annie.sophia219@gmail.com>
2021-04-22 12:39:05 -0400

    fix(render):Single Product View Rendered

--- client/store/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import singleProductReducer from './singleProduct';
-
-const rootReducer = combineReducers({
-  product: singleProductReducer,
-});
-
-export default rootReducer;
